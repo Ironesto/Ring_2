81d224b733c556660a792837e1b70dbbf919b4ab
Ironesto <gabriel.p.g@hotmail.es>
2024-01-18 04:32:54 +0100

    routesplit

--- pipex/main.c
+++ pipex/main.c
@@ -6,14 +6,52 @@
 /*   By: gpaez-ga <gpaez-ga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 18:40:35 by gpaez-ga          #+#    #+#             */
-/*   Updated: 2024/01/17 20:34:09 by gpaez-ga         ###   ########.fr       */
+/*   Updated: 2024/01/18 04:30:50 by gpaez-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+char **ft_routes(char **envp)
+{
+	int	i;
+	int	k;
+	char **rout;
+
+	i = 0;
+	k = -1;
+	while (ft_strncmp(envp[i], "PATH=", 5))
+		i++;
+	rout = ft_split(envp[i], ':');
+	rout[0] = &rout[0][5];
+	while (rout[++k])	
+		rout[k] = ft_strjoin(rout[k], "/");
+	return (rout);
+}
+
+static char	**ft_free(char **str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	while (i >= 0)
+	{
+		free(str[i]);
+		i--;
+	}
+	free (str);
+	return (NULL);
+}
+
 int	main(int argc, char **argv, char **envp)
 {
-	
+	char **rout;
+
+	if (argc != 5)
+		return (1);		//gestionar error?
+	rout = ft_routes(envp);
+	ft_free(rout);
 	return (0);
 }
--- pipex/pipex.h
+++ pipex/pipex.h
@@ -6,16 +6,23 @@
 /*   By: gpaez-ga <gpaez-ga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 18:40:27 by gpaez-ga          #+#    #+#             */
-/*   Updated: 2024/01/13 18:42:18 by gpaez-ga         ###   ########.fr       */
+/*   Updated: 2024/01/18 04:31:52 by gpaez-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
+	//printf
 # include <stdio.h>
+
+	//write
 # include <unistd.h>
+
+	//libft
 # include "./libft/libft.h"
 
+	//open
+# include <fcntl.h>
 
 #endif
